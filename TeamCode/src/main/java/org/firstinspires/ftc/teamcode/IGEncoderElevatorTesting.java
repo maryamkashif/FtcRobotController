6c3b0ca9ffecab75c845ea4a0f76dec936557a7c
maryamkashif <maryamkashif.b@gmail.com>
2024-10-04 11:13:34 -0500

    Update IGEncoderElevatorTesting.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/IGEncoderElevatorTesting.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/IGEncoderElevatorTesting.java
@@ -6,6 +6,11 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 
 @TeleOp
 public class IGEncoderElevatorTesting extends LinearOpMode {
+
+    //elevator motors
+    DcMotor elevatorRight = hardwareMap.dcMotor.get("elevatorRight");
+    DcMotor elevatorLeft = hardwareMap.dcMotor.get("elevatorLeft");
+
     @Override
     public void runOpMode() throws InterruptedException {
         // Position of the arm when it's lifted
@@ -15,11 +20,6 @@ public class IGEncoderElevatorTesting extends LinearOpMode {
 
         // Position of the arm when it's down
         int downpos = 0;
-
-        // elevators
-        DcMotor elevatorRight = hardwareMap.dcMotor.get("elevatorRight");
-        DcMotor elevatorLeft = hardwareMap.dcMotor.get("elevatorLeft");
-
 
         // Reset the motor encoder so that it reads zero ticks
         elevatorRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
@@ -34,56 +34,39 @@ public class IGEncoderElevatorTesting extends LinearOpMode {
         while (opModeIsActive()) {
             // all the way down
             if (gamepad1.dpad_down) {
-                elevatorRight.setTargetPosition(downpos);
-                elevatorLeft.setTargetPosition(downpos);
-                elevatorRight.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-                elevatorLeft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-                elevatorRight.setPower(0.5);
-                elevatorLeft.setPower(0.5);
+                elevator(downpos, 0.5);
             }
 
             else if (gamepad1.dpad_left) {
-                elevatorRight.setTargetPosition(pos1);
-                elevatorLeft.setTargetPosition(pos1);
-                elevatorRight.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-                elevatorLeft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-                elevatorRight.setPower(0.5);
-                elevatorLeft.setPower(0.5);
+                elevator(pos1, 0.5);
             }
 
             else if (gamepad1.dpad_up) {
-                elevatorRight.setTargetPosition(pos2);
-                elevatorLeft.setTargetPosition(pos2);
-                elevatorRight.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-                elevatorLeft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-                elevatorRight.setPower(0.5);
-                elevatorLeft.setPower(0.5);
+                elevator(pos2, 0.5);
             }
 
             else if (gamepad1.dpad_right) {
-                elevatorRight.setTargetPosition(pos3);
-                elevatorLeft.setTargetPosition(pos3);
-                elevatorRight.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-                elevatorLeft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-                elevatorRight.setPower(0.5);
-                elevatorLeft.setPower(0.5);
+                elevator(pos3, 0.5);
             }
 
             // Get the current position of the armMotor
             double elevatorRightPos = elevatorRight.getCurrentPosition();
             double elevatorLeftPos = elevatorLeft.getCurrentPosition();
 
-            // Get the target position of the armMotor
-            //double desiredPosition = armMotor.getTargetPosition();
-
             // Show the position of the armMotor on telemetry
             telemetry.addData("Right elevator Encoder Position", elevatorRightPos);
             telemetry.addData("Left elevator Encoder Position", elevatorLeftPos);
 
-            // Show the target position of the armMotor on telemetry
-            //telemetry.addData("Desired Position", desiredPosition);
-
             telemetry.update();
         }
+    }
+
+    public void elevator(int pos, double power) {
+        elevatorRight.setTargetPosition(pos);
+        elevatorLeft.setTargetPosition(pos);
+        elevatorRight.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        elevatorLeft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        elevatorRight.setPower(power);
+        elevatorLeft.setPower(power);
     }
 }
